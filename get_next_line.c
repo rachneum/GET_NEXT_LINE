dcacd3f98d26d70c4cceccc3ef057a73d04be527
rachneum <rachel-1604@hotmail.com>
2023-11-15 16:26:26 +0100

    6

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,36 @@
 /*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:09:19 by rachou            #+#    #+#             */
-/*   Updated: 2023/11/09 16:40:28 by rachou           ###   ########.fr       */
+/*   Updated: 2023/11/15 16:14:50 by rachou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	polish_list(t_list **list)//polish: special function that is taking care of the chars that were not used int the previous gnl.
+{
+	t_list	*last_node;
+	t_list	*clean_node;
+	int		i;
+	int		k;
+	char	*buf;
+
+	buf = malloc(BUFFER_SIZE + 1);//in this buffer i'll put the last chars of the last node.
+	clean_node = malloc(sizeof(t_list));
+	if (NULL == buf || NULL = clean_node)
+		return ;
+	last_node = find_last_node(*list);
+	i = 0;
+	k = 0;
+	while (last_node->str_buf[i] && last_node->str_buf[i] != '\n')
+		i++;
+	while (last_node->str_buf[i] && last_node->str_buf[++i])
+		buf[k++] = last_node->str_buf[i];
+	buf[k] = '\0';
+	clean_node->str_buf = buf;
+	clean_node->next = NULL;
+	dealloc(list, clean_node, buf);
+}
 
 char	*get_line(t_list *list)
 {
@@ -69,9 +94,9 @@ void	create_list(t_list **list, int fd)
 //it has to fetch a specific string from a specific file
 char	*get_next_line(int fd)
 {//static: variable that behaves like a global variable with local scope, only gnl can use this variable.
-	static t_list	*list = NULL;
+	static t_list	*list = NULL;//static: is gonna save his state in between the calls.
 	char			*next_line;//i'll stock the next line i'm gonna take.
-//fd are only positives.			read gives -1 if some problems on reading.
+	//fd are only positives.		  read gives -1 if some problems on reading.
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
 		return (NULL);
 	create_list(&list, fd);//create my list until i stumble into '\n'.
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,12 +6,67 @@
 /*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:09:59 by rachou            #+#    #+#             */
-/*   Updated: 2023/11/09 16:41:08 by rachou           ###   ########.fr       */
+/*   Updated: 2023/11/15 16:25:37 by rachou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
+
+int	found_newline(t_list *list)
+{
+	int	i;
+
+	if (NULL == list)
+		return (0);
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i] && i < BUFFER_SIZE)
+		{
+			if (list->str_buf[i] == '\n')
+				return (1);
+			++i;
+		}
+		list = list->next;
+	}
+	return (0);
+}
+
+t_list	*find_last_node(t_list	*list)
+{
+	if (NULL == list)
+		return (NULL);
+	while (list->next)
+		list = list->next;
+	return (list);
+}
+
+void	copy_str(t_list *list, char *str)
+{
+	int	i;
+	int	k;
+
+	if (NULL == list)
+		return ;
+	k = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])//until i have a char wich is different from 0.
+		{
+			if (list->str_buf[i] == '\n')//check if the char pointed by i is equal to '\n'.
+			{
+				str[k++] = '\n';
+				str[k] = '\0';
+				return ;
+			}
+			str[k++] = list->str_buf[i++];//just copy the string.
+		}
+		list = list->next;//go to the next node.
+	}
+	str[k] = '\0';
+}
 
 int	len_to_newline(t_list *list)
 {
@@ -38,28 +93,26 @@ int	len_to_newline(t_list *list)
 	}
 	return (len);
 }
-void	copy_str(t_list *list, char *str)
-{
-	int	i;
-	int	k;
 
-	if (NULL == list)
+void	dealloc(t_list **list, t_list *clean_node, char *buf)
+{
+	t_list	*tmp;
+
+	if (NULL == *list)
 		return ;
-	k = 0;
-	while (list)
+	while (*list)
 	{
-		i = 0;
-		while (list->str_buf[i])//until i have a char wich is different from 0.
-		{
-			if (list->str_buf[i] == '\n')//check if the char pointed by i is equal to '\n'.
-			{
-				str[k++] = '\n';
-				str[k] = '\0';
-				return ;
-			}
-			str[k++] = list->str_buf[i++];//just copy the string.
-		}
-		list = list->next;//go to the next node.
+		tmp = (*list)->next;
+		free((*list)->str_buf);
+		free(*list);
+		*list = tmp;
 	}
-	str[k] = '\0';
+	*list = NULL;
+	if (clean_node->str_buf[0])
+		*list = clean_node;
+	else
+	{
+		free(buf);
+		free(clean_node);
+	}
 }
